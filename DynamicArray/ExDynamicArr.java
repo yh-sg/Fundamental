cfcb0f2b139a5f6ad629b604550210d7f91f71ae
yonghan <wangyh.92@gmail.com>
2021-02-16 21:24:07 +0800

    secondcommit

--- DynamicArray/ExDynamicArr.java
+++ DynamicArray/ExDynamicArr.java
@@ -1,7 +1,9 @@
 package DynamicArray;
 
+//Similar to ArrayList in Java!
+
 @SuppressWarnings("unchecked")
-public class ExDynamicArr <T> implements Iterable <T> {
+public class ExDynamicArr<T> implements Iterable<T> {
 
     private T [] arr;
     private int len = 0; //length user thinks the array is
@@ -40,5 +42,58 @@ public class ExDynamicArr <T> implements Iterable <T> {
         len=0;
     }
 
+    public void add(T elem){
+        //Resizing
+        if(len+1>=capacity){
+            if(capacity==0)
+                capacity=1; //Default
+            else
+                capacity*=2; //Double the size!
+            T[] new_arr = (T[]) new Object[capacity];
+            for (int i = 0; i < len; i++) 
+                new_arr[i] = arr[i];
+            arr = new_arr; //With additional nulls padded
+        }
+        arr[len++] = elem;
+    }
+
+    public T removeAt(int rm_index){
+        if(rm_index >= len || rm_index < 0)
+            throw new IndexOutOfBoundsException();
+        T data = arr[rm_index];
+        T[] new_arr = (T[]) new Object[len - 1];
+        
+        for (int i = 0, j = 0 ; i < len; i++, j++) 
+            if(i==rm_index)
+                j--;
+            else
+                new_arr[j]=arr[i];
+            arr=new_arr;
+            capacity = --len;
+            return data;
+    }
+
+  // Iterator is still fast but not as fast as iterative for loop
+  @Override
+  public java.util.Iterator<T> iterator() {
+    return new java.util.Iterator<T>() {
+      int index = 0;
+
+      @Override
+      public boolean hasNext() {
+        return index < len;
+      }
+
+      @Override
+      public T next() {
+        return arr[index++];
+      }
+
+      @Override
+      public void remove() {
+        throw new UnsupportedOperationException();
+      }
+    };
+  }
     
 }
