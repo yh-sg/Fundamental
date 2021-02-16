a15a24c2a463d0b199fb3be2b3807883b5440214
yonghan <wangyh.92@gmail.com>
2021-02-17 07:01:40 +0800

    LL unshift

--- LinkedLists/linkedList.js
+++ LinkedLists/linkedList.js
@@ -73,19 +73,31 @@ class SinglyLinkedList{
     shift = () => {
         if(this.length===0)
             return undefined
-        let shiftHead = this.head
+        let currentHead = this.head
         this.head = this.head.next
         // console.log(this.head)
         this.length--
-        return shiftHead
+        if(this.length==0){
+            this.tail = null
+        }
+        return currentHead
     }
 
+    unshift = (val) => {
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }else{
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+    
 }
 
 let list = new SinglyLinkedList()
-list.push(99)
-list.push(98)
-list.traverse()
-// console.log(list);
-list.shift()
-list.traverse()
+console.log(list.unshift(77))
+console.log(list);
