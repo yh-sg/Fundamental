8d8db55ab5eeeb66aaa5eb9a9b04793af2f41875
yonghan <wangyh.92@gmail.com>
2021-02-18 21:00:11 +0800

    DLL done

--- LinkedLists/doublyLinkedList.js
+++ LinkedLists/doublyLinkedList.js
@@ -92,7 +92,7 @@ class DoubleLinkedList{
 
     get = (index) => {
         if(index<0||index>=this.length)
-            return undefined
+            return null
         let current = this.head
         let currentLength = 0
         while(index!=currentLength){
@@ -101,6 +101,59 @@ class DoubleLinkedList{
         }
         return current
     }
+
+    set = (val, index) => {
+        let foundNode = this.get(index)
+        if(foundNode!=null){
+            foundNode.val = val;
+            return this
+        }
+        return undefined
+    }
+
+    insert = (val, index) => {
+        if(index<0||index>this.length)
+            return undefined
+        if(this.length===0)
+            return this.unshift(val)
+        else if(this.length===index)
+            return this.push(val)
+        else{
+            let newNode = new Node(val)
+            let prevNode = this.get(index-1)
+            let nextNode = this.get(index)
+            prevNode.next = newNode
+            newNode.prev = prevNode
+            newNode.next = nextNode
+            nextNode.prev = newNode
+            this.length++
+            return this
+        }
+    }
+
+    remove = (index) => {
+        if(index<0||index>=this.length)
+            return undefined
+
+        if(index===0)
+            return this.shift()
+        else if(index=this.length-1)
+            return this.pop()
+        else if(index=0)
+            return this.shift()
+        else{
+            let removeNode = this.get(index)
+            let prevNode = this.get(index-1)
+            let nextNode = this.get(index+1)
+            removeNode.next = null
+            removeNode.prev = null
+            prevNode.next = nextNode
+            nextNode.prev = prevNode
+            this.length--
+            return removeNode
+        }
+    }
+
 }
 
 const DLL = new DoubleLinkedList()
