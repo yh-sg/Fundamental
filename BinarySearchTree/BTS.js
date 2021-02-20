b59944a2d8cd6c4531efb9b94b3961cdaf220365
yonghan <wangyh.92@gmail.com>
2021-02-20 19:50:13 +0800

    BTS search

--- BinarySearchTree/BTS.js
+++ BinarySearchTree/BTS.js
@@ -39,12 +39,21 @@ class BinarySearchTree{
     }
 
     search = (val) => {
-        if(this.root===null) return null;
+        if(this.root===null) return false;
         else{
             let current = this.root
-            while(true){
-                return null
+            let found = false
+            while(current && !found){
+                if(val < current.val){
+                    current = current.left;
+                }else if(val > current.val){
+                    current = current.right
+                }else{
+                    found = true
+                }
             }
+            if(!found) return undefined
+            return current;
         }
     }
 }
@@ -53,11 +62,11 @@ class BinarySearchTree{
 //   5     13
 // 2  7  11  16
 
-// let tree = new BinarySearchTree();
-// tree.insert(10)
-// tree.insert(5)
-// tree.insert(13)
-// tree.insert(11)
-// tree.insert(2)
-// tree.insert(16)
-// tree.insert(7)
+let tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
