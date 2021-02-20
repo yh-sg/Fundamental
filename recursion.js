f77887f0ecdf23ef763a0079ddada57fb30fcb7e
yonghan <wangyh.92@gmail.com>
2021-02-20 21:01:35 +0800

    recursion

---
+++ recursion.js
@@ -0,0 +1,64 @@
+//* 1. Base Case 
+//? 2. Different input
+//!  No base case and return wrong thing could cause stack overflow
+
+function countDown(num){
+    //TODO Base Case
+    if(num==0){
+        console.log('all done');
+        return;
+    }else{
+        console.log(num);
+        countDown(num-1)
+    }
+}
+
+function sumRange(num) {
+    //TODO Base Case
+    if(num===1) return 1;
+    return num + sumRange(num-1)
+}
+
+function factorial(num){
+    if(num===1) return 1
+    return num * factorial(num-1)
+}
+
+function power(base,exponent){
+    if(exponent===0) return 1
+    return base * power(base,exponent-1)
+}
+
+function productOfArr(arr){
+    // productOfArray([1,2,3]) // 6
+    // productOfArray([1,2,3,10]) // 60
+    if(arr.length===0) return 1
+    return arr[0] * productOfArr(arr.slice(1))
+}
+
+function recursiveRange(num){
+    if(num===1) return 1
+    return num + recursiveRange(num-1)
+}
+
+function fib(num){
+    if(num<=2) return 1
+    return fib(num-2) + fib(num-1)
+}
+
+function reverse(str){
+    if(str.length === 0) return str;
+	return reverse(str.slice(1))+str[0];
+}
+
+function isPalindrome(str){
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0]===str[1]
+    if(str[0]===str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false
+}
+
+function someRecursive(arr, callback){
+    //TODO
+    // if(arr.length===0) return false
+  }
