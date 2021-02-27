6feef35027b52b33f6111c360af7db9a2136f820
yonghan <wangyh.92@gmail.com>
2021-02-27 16:59:15 +0800

    DFS graph

--- Graphs/Graphs.js
+++ Graphs/Graphs.js
@@ -30,6 +30,25 @@ class Graph{
         }
         delete this.adjacencyList[vertex]
     }
+
+    DFS(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor)
+                }
+            });
+        })(start);
+
+        return result;
+    }
 }
 
 let g = new Graph()
