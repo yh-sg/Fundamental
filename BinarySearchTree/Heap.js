ce1436dc2cedcdc312b6ee3aa4a53eb9c57fba6b
yonghan <wangyh.92@gmail.com>
2021-03-02 06:02:41 +0800

    extract max heap

--- BinarySearchTree/Heap.js
+++ BinarySearchTree/Heap.js
@@ -14,6 +14,47 @@ class MaxBinaryHeap{
             parentIndex = Math.floor((index-1)/2);
         }
     }
+
+    extractMax(){
+        let max=this.arr[0],
+            end = this.arr.pop();
+        if(this.arr.length>0){
+            this.arr[0] = end;
+            this.sinkDown();
+        }
+        //[55, 39, 41, 18, 27, 12, 33] original value
+        //![33, 39, 41, 18, 27, 12]
+        return max
+    }
+
+    sinkDown(){
+        let index = 0;
+        const length = this.arr.length,
+            element = this.arr[0];
+        while(true){
+            //Becareful of out of bound. Will crash
+            let leftChildIndex = 2*index+1,
+                rightChildIndex = 2*index+2,
+                leftChild,
+                rightChild,
+                swap=null;
+
+            if(leftChildIndex < length){ //check if it's inbound or not
+                leftChild = this.arr[leftChildIndex];
+                if(leftChild>element) swap = leftChildIndex;
+            }
+
+            if(rightChildIndex < length){ //check if it's inbound or not
+                rightChild = this.arr[rightChildIndex];
+                if((swap!==null&&rightChild>leftChild) || (swap===null&&rightChild>element))
+                    swap = rightChildIndex;
+            }
+            if(swap===null) break;
+            this.arr[index] = this.arr[swap];
+            this.arr[swap] = element;
+            index = swap
+        }
+    }
 }
 
 let heap = new MaxBinaryHeap();
