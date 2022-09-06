7582bceab0af59217ce770bb2ceb4756e45006e1
yonghan <wangyh.92@gmail.com>
2022-09-06 18:38:03 +0800

    BST naive solution for insert, lookup and comments for remove methods

--- DataStructure/Trees/BST.ts
+++ DataStructure/Trees/BST.ts
@@ -1,7 +1,7 @@
 interface IBinarySearchTree{
     insert(val:number):void
-    lookup(val:number):void
-    remove(val:number):void
+    lookup(val:number):TreeNode|undefined
+    remove(val:number):TreeNode|undefined
 }
 
 class TreeNode{
@@ -23,12 +23,79 @@ class BinarySearchTreePractice implements IBinarySearchTree{
         this.root=null
     }
     insert(val: number): void {
-        throw new Error("Method not implemented.");
+        let newNode = new TreeNode(val);
+        if(!this.root){
+            this.root=newNode;
+            return;
+        }
+
+        let current=this.root;
+
+        while(true){
+            if(current.val===val) throw new Error("BST should not have node same value"); //if similar value, break
+
+            //compare the node value, if larger goes right, otherwise goes left
+            if(val<current.val){
+                if(!current.left){
+                    current.left=newNode
+                    return;
+                }else{
+                    current=current.left!;
+                    continue;
+                }
+                    
+            }
+            if(current.val<val){
+                if(!current.right){
+                    current.right=newNode
+                    return;
+                }else{
+                    current=current.right!;
+                    continue;
+                } //continue the loop and compare, if it's not null. Otherwise, insert node and exit.   
+            }
+        } 
     }
-    lookup(val: number): void {
-        throw new Error("Method not implemented.");
+    lookup(val: number): TreeNode|undefined {
+        if(!this.root) throw new Error("It's an empty tree."); // Check root, if empty throw error
+
+        let current = this.root;
+
+        while(true){
+
+            if(!current) return undefined;
+
+            if(current.val===val) return current;
+
+            if(val<current.val){
+                current = current.left!;
+                continue;
+            }
+
+            if(val>current.val){
+                current = current.right!;
+                continue;
+            }
+
+            else undefined;
+        }// compare the node value, if larger goes right, otherwise goes left and continue until it reaches null
+
     }
-    remove(val: number): void {
+    remove(val: number): TreeNode|undefined {
+        //!Comments
+
+        // if only one node, remove that. -> Result 1
+        // throw error if empty tree -> Result 2
+        // Affected nodes: prevNode, removedNode, currentNode(Swapping value and remove linkage)
+        // Find removedNode, if cannot find throw error
+        // Removed node must always find the higher value first, which is on the right
+        // Right side and left side got nothing, destory the previous node linkage -> Result 3
+        // left side got something and right side got nothing, remove the linkage of previous node -> Result 4
+        // Otherwise, move to right side
+        // If left side got nothing, replace the val and destory the removedNode linkage -> Result 5
+        // Result 4: go left and if there's left node, keep on looping until the end.
+        // replace the value with removedNode and remove it's linkage -> Result 6
+
         throw new Error("Method not implemented.");
     }
 }
@@ -40,8 +107,37 @@ interface NodeObject{
 }
 
 const traverse = (node:TreeNode):NodeObject => {
+    if(!node) throw new Error("No tree to traverse");
     const tree:NodeObject = {value: node.val, left:null, right:null}
     tree.left = node.left === null ? null : traverse(node.left);
     tree.right = node.right === null ? null : traverse(node.right);
     return tree;
 }
+
+const tree = new BinarySearchTreePractice();
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+console.log(tree.lookup(9))
+console.log(tree.lookup(4))
+console.log(tree.lookup(6))
+console.log(tree.lookup(20))
+console.log(tree.lookup(170))
+console.log(tree.lookup(15))
+console.log(tree.lookup(1))
+console.log(tree.lookup(0))
+
+console.log(JSON.stringify(traverse(tree.root!)))
+
+/* 
+                9
+        4               20
+    1       6       15      170
+*/
+
+
+
