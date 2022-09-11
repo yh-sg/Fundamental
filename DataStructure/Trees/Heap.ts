7b697b7edd4491e9cb1f07c347e0b6881ddb8148
yonghan <wangyh.92@gmail.com>
2022-09-11 19:16:44 +0800

    implemented heap insert

--- DataStructure/Trees/Heap.ts
+++ DataStructure/Trees/Heap.ts
@@ -1,4 +1,9 @@
-interface ICommonHeapMethod{
+interface ISwapMethod{
+    HeapArr:number[];
+    swap(temp:number, element:number):void
+}
+
+interface ICommonHeapMethod extends ISwapMethod{
     insert(element:number):number[];
 }
 
@@ -10,28 +15,75 @@ interface IMaxHeap extends ICommonHeapMethod{
     extractMax():number[];
 }
 
-class MaxBinaryHeapImpl implements IMaxHeap{
-    private arr:number[];
+class HeapSwapImpl implements ISwapMethod{
+    HeapArr:number[];
     constructor(){
-        this.arr=[];
+        this.HeapArr=[];
+    }
+
+    swap(temp: number, elementIndex: number): void {
+        [this.HeapArr[temp],this.HeapArr[elementIndex]] = [this.HeapArr[elementIndex], this.HeapArr[temp]]
+    }
+
+}
+
+class MaxBinaryHeapImpl extends HeapSwapImpl implements IMaxHeap{
+    constructor(){
+        super();
     }
     insert(element: number): number[] {
-        throw new Error("Method not implemented.");
+        //push to last slot in array
+        this.HeapArr.push(element)
+
+        //Declare index of this pushed array and the parent array of the heap
+        let index = this.HeapArr.length - 1,
+            parentIndex = Math.floor((index-1)/2);  //left(all odd number), right(all even number) 
+
+        //If higher than parentIndex, do swapping until no longer higher.
+        while(this.HeapArr[parentIndex]<this.HeapArr[index]){
+            this.swap(parentIndex, index);
+            //!Set index to parentIndex and parentIndex to new parentIndex, loop again until equal or lesser
+            index=parentIndex;
+            parentIndex=Math.floor((index-1)/2);
+        }
+
+        return this.HeapArr;
     }
     extractMax(): number[] {
         throw new Error("Method not implemented.");
     }
 }
 
-class MinBinaryHeapImpl implements IMinHeap{
-    private arr:number[];
+class MinBinaryHeapImpl extends HeapSwapImpl implements IMinHeap{
     constructor(){
-        this.arr=[];
+        super();
     }
     insert(element: number): number[] {
-        throw new Error("Method not implemented.");
+        //* Similar as above, just while statement different condition.
+        this.HeapArr.push(element)
+
+        let index = this.HeapArr.length - 1,
+            parentIndex = Math.floor((index-1)/2); 
+
+        while(this.HeapArr[parentIndex]>this.HeapArr[index]){
+            this.swap(parentIndex, index);
+            index=parentIndex;
+            parentIndex=Math.floor((index-1)/2);
+        }
+
+        return this.HeapArr;
     }
     extractMin(): number[] {
         throw new Error("Method not implemented.");
     }
 }
+
+// let heap = new MaxBinaryHeapImpl();
+let heap = new MinBinaryHeapImpl();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12)
+console.log(heap.insert(55));
