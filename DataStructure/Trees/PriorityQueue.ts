df96378f2b61ed66fd76336931f3b38de276e522
yonghan <wangyh.92@gmail.com>
2022-09-11 16:19:56 +0800

    complete priorityQueue

--- DataStructure/Trees/PriorityQueue.ts
+++ DataStructure/Trees/PriorityQueue.ts
@@ -8,12 +8,13 @@ enum Priority{
 
 interface IPriorityQueue{
     enqueue(desc:string, priority:Priority):PriorityQueueNode[];
+    dequeue():PriorityQueueNode[];
 }
 
 class PriorityQueueNode {
 
-    protected desc:string;
-    protected priority:Priority;
+    desc:string;
+    priority:Priority;
 
     constructor(desc, priority) {
         this.desc = desc;
@@ -30,6 +31,31 @@ class PriorityQueueImpl implements IPriorityQueue{
     }
 
     enqueue(desc: string, priority: Priority): PriorityQueueNode[] {
-        throw new Error("Method not implemented.");
+        let newNode = new PriorityQueueNode(desc, priority);
+        
+        //Loop and check each priority, if finally found where it should insert, splice it and return the queue
+        for (let i=0;i<this.priorityQueue.length;i++) {
+            if(newNode.priority<this.priorityQueue[i].priority){
+                this.priorityQueue.splice(i,0,newNode);
+                return this.priorityQueue;
+            }
+        }
+
+        //If it's end of loop yet nothing occurs, push it in and return the queue
+        this.priorityQueue.push(newNode)
+        return this.priorityQueue;
+    }
+
+    dequeue(): PriorityQueueNode[]{
+        this.priorityQueue.shift();
+        return this.priorityQueue;
     }
 }
+
+let EmergencyRm = new PriorityQueueImpl();
+EmergencyRm.enqueue("common cold", Priority.COMMON);
+EmergencyRm.enqueue("gunshot wound", Priority.SEVERE);
+EmergencyRm.enqueue("high fever", Priority.ALARMING);
+EmergencyRm.enqueue("broken arm", Priority.SERIOUS);
+EmergencyRm.enqueue("glass in foot", Priority.MILD);
+console.log(EmergencyRm.enqueue("knife stab", Priority.SEVERE));
