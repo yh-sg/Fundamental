3edf1e1e0876fd46f6aef798131222a9e1bc9ca7
yonghan <wangyh.92@gmail.com>
2022-10-21 20:45:58 +0800

    recusion fabonacci and factorial naive solution

--- Algorithm/Recursion/recursion.ts
+++ Algorithm/Recursion/recursion.ts
@@ -8,7 +8,45 @@ const findFactorialIterative = (num:number):number => {
 }
 
 const findFactorialRecursive = (num:number):number => {
-    return 0;
+    // 1. Identify the base case
+    numberValidator(num);
+    let base = num,
+        count = num;
+
+    if(count===1)
+        return base;
+
+    // 2. Identify the recursive case
+    // 3. Get closer and closer and return. 
+    return base *= findFactorialRecursive(count-1);
+}
+
+const fabonacciIterative = (num:number):number => {
+    
+    if(num<=0) return 0;
+
+    if(num===1 || num===2) return 1;
+
+    let arr = [0,1,1]
+
+    for (let i = 3; i <= num; i++) {
+        arr.push(arr[i-1]+arr[i-2]);
+    }
+
+    return arr[num];
+}
+
+const fabonacciRecursive = (num:number):number => {
+    
+    // 1. Identify the base case
+    if(num<=0) return 0;
+    if(num===1 || num===2) return 1;
+
+    let result = 1;
+    // 2. Identify the recursive case
+    // 3. Get closer and closer and return. 
+
+    return result = fabonacciRecursive(num-1) + fabonacciRecursive(num-2);
 }
 
 const numberValidator = (num:number):void => {
@@ -25,6 +63,6 @@ const reverseStr = (str:string):string => {
     return "";
 }
 
-const isPalindrome = (str:string):boolean => {
+const isPalindrome = (str:string):boolean =>{
     return false;
 }
