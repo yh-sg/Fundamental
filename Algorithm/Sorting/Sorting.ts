268a5e41d803045ff79ec82959ccbca314b4dc5c
yonghan <wangyh.92@gmail.com>
2022-10-26 21:19:10 +0800

    Algo_EmptyTemplateSorting

---
+++ Algorithm/Sorting/Sorting.ts
@@ -0,0 +1,30 @@
+let arr = [6,5,3,1,8,7,2,4];
+
+const bubbleSort = (arr:number[]):number[] => {
+
+    return arr;
+}
+
+const selectionSort = (arr:number[]):number[] => {
+
+    
+    return arr;
+}
+
+const insertionSort = (arr:number[]):number[] => {
+
+    
+    return arr;
+}
+
+const quickSort = (arr:number[]):number[] => {
+
+    
+    return arr;
+}
+
+const mergeSort = (arr:number[]):number[] => {
+
+    
+    return arr;
+}
