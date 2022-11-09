e45487905ba1d4e4a34793c728a2a4f8243490cf
yonghan <wangyh.92@gmail.com>
2022-11-09 11:32:19 +0800

    Algo_EmptyTemplateSearching

---
+++ Algorithm/searching.ts
@@ -0,0 +1,17 @@
+const linearSearch = (arr:string[]|number[], val:string|number):string => {
+    let result;
+    return `Value ${val} was found in position ${result} for searching in ${arr}`
+};
+
+const binarySearch = (arr:string[]|number[], val:string|number):string => {
+    let result;
+    return `Value ${val} was found in position ${result} for searching in ${arr}`
+};
+
+const BFS = () => {
+    
+};
+
+const DFS = () => {
+    
+};
